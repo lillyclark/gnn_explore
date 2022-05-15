a4b2c1cf8fd1733740c7c045e25b83d927b75df8
lilliamc <lilliamc@usc.edu>
2022-05-15 16:53:57 -0700

    named features

--- Environments.py
+++ Environments.py
@@ -4,18 +4,27 @@ from torch_geometric.data import Data
 
 class GraphEnv():
     def __init__(self):
-        self.num_robots = 2
-        self.num_actions = 3 # left, stay, right
+        self.IS_ROBOT = 0
+        self.IS_ROBOT_GOAL = 1
         self.num_node_features = 2 # is_robot, is_goal
-        self.feature_matrix = torch.Tensor([[1,0],[0,0],[0,0],[0,1],[1,0],[0,0],[0,0],[0,1]])
-        self.edge_index = torch.Tensor([[0,1,2,3,4,5,6],[1,2,3,4,5,6,7]]).long()
-        self.edge_attr = torch.Tensor([1,1,1,1,1,1,1])
-        self.goal_feature_matrix = torch.Tensor([[0,0],[0,0],[0,0],[1,1],[0,0],[0,0],[0,0],[1,1]])
+
+        self.num_actions = 3 # each node has max 2 edges
+        self.num_nodes = 8
+
+        self.feature_matrix = torch.zeros((self.num_nodes, self.num_node_features))
+        self.feature_matrix[0][self.IS_ROBOT], self.feature_matrix[4][self.IS_ROBOT] = True, True
+        self.feature_matrix[3][self.IS_ROBOT_GOAL], self.feature_matrix[7][self.IS_ROBOT_GOAL] = True, True
+
+        self.goal_feature_matrix = torch.zeros(self.feature_matrix.shape)
+        self.goal_feature_matrix[3][self.IS_ROBOT], self.goal_feature_matrix[7][self.IS_ROBOT] = True, True
+        self.goal_feature_matrix[3][self.IS_ROBOT_GOAL], self.goal_feature_matrix[7][self.IS_ROBOT_GOAL] = True, True
+
+        self.edge_index = torch.Tensor([list(range(0,self.num_nodes-1)),list(range(1,self.num_nodes))]).long()
+        self.edge_attr = torch.ones(self.num_nodes-1)
         self.state = Data(x=self.feature_matrix, edge_index=self.edge_index, edge_attr=self.edge_attr)
 
     def reset(self):
-        self.feature_matrix = torch.Tensor([[1,0],[0,0],[0,0],[0,1],[1,0],[0,0],[0,0],[0,1]])
-        self.state = Data(x=self.feature_matrix, edge_index=self.edge_index, edge_attr=self.edge_attr)
+        self.__init__()
         return self.state
 
     def incident_edges(self, node_idx): # list of tuples
@@ -29,25 +38,19 @@ class GraphEnv():
 
     def step(self,action):
         new_feature_matrix = torch.zeros(self.feature_matrix.shape)
-        new_feature_matrix[:,1] = self.feature_matrix[:,1]
-        for n in range(self.feature_matrix.size()[0]):
-            if self.feature_matrix[n][0]: # is robot
+        new_feature_matrix[:,self.IS_ROBOT_GOAL] = self.feature_matrix[:,self.IS_ROBOT_GOAL]
+        for n in range(self.feature_matrix.size()[self.IS_ROBOT]):
+            if self.feature_matrix[n][self.IS_ROBOT]: # is robot
                 u, v = self.incident_edges(n)[action[n]]
                 if u == n and v == n: # stay
-                    new_feature_matrix[v][0] = 1
-                elif u == n and not self.feature_matrix[v][0] and not new_feature_matrix[v][0]:
-                    new_feature_matrix[v][0] = 1
-                elif v == n and not self.feature_matrix[u][0] and not new_feature_matrix[u][0]:
-                    new_feature_matrix[u][0] = 1
+                    new_feature_matrix[v][self.IS_ROBOT] = True
+                elif u == n and not self.feature_matrix[v][self.IS_ROBOT] and not new_feature_matrix[v][self.IS_ROBOT]:
+                    new_feature_matrix[v][self.IS_ROBOT] = True
+                elif v == n and not self.feature_matrix[u][self.IS_ROBOT] and not new_feature_matrix[u][self.IS_ROBOT]:
+                    new_feature_matrix[u][self.IS_ROBOT] = True
                 else:
-                    new_feature_matrix[n][0] = 1 # stay
+                    new_feature_matrix[n][self.IS_ROBOT] = True # wanted to move but couldn't
         self.feature_matrix = new_feature_matrix
-        if self.feature_matrix[:,0].sum() != self.num_robots:
-            print(self.feature_matrix[:,0])
-            raise ValueError
-        if self.feature_matrix[:,1].sum() != 2:
-            print(self.feature_matrix[:,1])
-            raise ValueError
         self.state = Data(x=self.feature_matrix, edge_index=self.edge_index, edge_attr=self.edge_attr)
         reward, done = self.get_reward()
         return self.state, reward, done, None
