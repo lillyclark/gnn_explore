01ce10e32e58778e4703a025f8e225d0309f9630
lilliamc <lilliamc@usc.edu>
2022-06-08 16:10:06 -0700

    with a smaller environment, policy gradient + GCN works, and A2C + GCNA2C works

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import torch
 from Networks import *
 from Environments import TestEnv, GraphEnv
 from Policies import *
+import wandb
 
 np.random.seed(0)
 torch.manual_seed(0)
@@ -14,21 +15,24 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 mode = ['train','test']
 # mode = ['train','test','write']
 
-RUN_NAME = "master_GGNN_2_"
+RUN_NAME = "GCN_A2C_"
 
-wandb.init(project="simple-world-2", entity="lillyclark", config={})
+wandb.init(project="smaller-world", entity="lillyclark", config={})
 wandb.run.name = RUN_NAME+wandb.run.id
 
 model_name = 'models/'+RUN_NAME+'_policy_gradient.pt'
 
-env = GraphEnv(reward_name = "base_reward", has_master = True)
+env = GraphEnv(reward_name = "base_reward", has_master = False)
 
 # actor = SimpleActor(env.num_node_features, env.num_nodes, env.num_actions).to(device)
 # actor = GCNActor(env.num_node_features, env.num_actions).to(device)
+actor = GCNA2C(env.num_node_features, env.num_actions).to(device)
 # actor = LinearAggActor(env.num_node_features, env.num_actions).to(device)
-actor = GGNNActor(env.num_node_features, env.num_actions).to(device)
+# actor = GGNNActor(env.num_node_features, env.num_actions).to(device)
 
-agent = PolicyGradient(device=device, n_iters=1, lr=0.001, gamma=0.99)
+# agent = PolicyGradient(device=device, n_iters=2000, lr=0.005, gamma=0.99)
+agent = A2C_Shared(device=device, n_iters=1000, lr=0.005, gamma=0.99)
+
 
 if 'read' in mode:
     n = torch.load(model_name)
@@ -36,10 +40,11 @@ if 'read' in mode:
 
 if 'train' in mode:
     agent.trainIters(env, actor, max_tries=500, plot=False)
-    wandb.finish()
 
 if 'write' in mode:
     torch.save(actor.state_dict(), model_name)
 
 if 'test' in mode:
     agent.play(env, actor, max_tries=100, v=True)
+
+wandb.finish()
