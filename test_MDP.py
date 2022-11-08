698ea05bdc5f487fb6f1126d488ab981eed9d016
lilliamc <lilliamc@usc.edu>
2022-11-07 18:30:03 -0800

    daniel's debug world: diamond and then two branches

--- test_MDP.py
+++ test_MDP.py
@@ -14,15 +14,20 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 #### DEFINE ENVIRONMENT
 num_nodes = 9
-right_i = torch.Tensor([0,0,1,2,3,4,5,6,6])
-right_j = torch.Tensor([1,2,3,4,5,5,6,7,8])
+right_i = torch.Tensor([0,0,1,2,3,4,4,5,6])
+right_j = torch.Tensor([1,2,3,3,4,5,6,7,8])
 env = ConfigureEnv(num_robots=2, num_actions=4, num_nodes=num_nodes, right_i=right_i, right_j=right_j)
 
-policy_name = "EnvA.p"
-model_name = "EnvA_gcn.pt"
+policy_name = "Env_daniel.p"
+model_name = "Env_daniel.pt"
+# model_name = "EnvA_simple.pt"
+
 
 # mode = ['write_policy']
-mode = ['read_policy', 'train', 'write', 'test']
+mode = ['read_policy']
+# mode = ['read_policy', 'train', 'write', 'test']
+# mode = ['read_policy', 'read', 'train', 'write', 'test']
+# mode = ['read_policy', 'train', 'test']
 
 if 'write_policy' in mode:
     transitions, rewards, visited_index, action_index = get_model(env)
@@ -32,9 +37,11 @@ if 'write_policy' in mode:
 
 if 'read_policy' in mode:
     visited_index, action_index, policy = load_optimal_policy(policy_name)
+    test_optimal_policy(env, visited_index, action_index, policy)
 
 # actor = SimpleActor(env.num_node_features, env.num_nodes, env.num_actions).to(device)
 actor = GCNActor(env.num_node_features, env.num_actions).to(device)
+actor.k = 2
 # actor = LinearAggActor(env.num_node_features, env.num_actions).to(device)
 # actor = GGNNActor(env.num_node_features, env.num_actions).to(device)
 
@@ -43,11 +50,14 @@ if 'read' in mode:
     actor.load_state_dict(n)
 
 if 'train' in mode:
-    wandb.init(project="MDP-medium-world", entity="lillyclark", config={})
-    wandb.run.name = model_name.split(".")[0]+"_"+wandb.run.id
+    wandb_log = False
+    if wandb_log:
+        wandb.init(project="MDP-medium-world", entity="lillyclark", config={})
+        wandb.run.name = model_name.split(".")[0]+"_"+wandb.run.id
     optimizer = optim.Adam(actor.parameters(), lr=0.001)
-    train_agent(env, actor, optimizer, visited_index, action_index, policy, max_tries=500, n_iters=1000)
-    wandb.finish()
+    train_agent(env, actor, optimizer, visited_index, action_index, policy, max_tries=500, n_iters=1000, wandb_log=wandb_log)
+    if wandb_log:
+        wandb.finish()
 
 if 'write' in mode:
     torch.save(actor.state_dict(), "models/"+model_name)
